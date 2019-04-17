fdba4e76ef77cb8b2fb37a72e1cb06aa3068bd45
arrossom <49291923+arrossom@users.noreply.github.com>
2019-04-17 17:19:17 -0500

    Add files via upload

---
+++ app/src/main/java/com/example/grocerybuddy/DeleteItemDialogFragment.java
@@ -0,0 +1,29 @@
+package com.example.grocerybuddy;
+
+import android.content.DialogInterface;
+import android.support.v4.app.DialogFragment;
+import android.app.Dialog;
+import android.app.AlertDialog;
+import android.os.Bundle;
+
+public class DeleteItemDialogFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog (Bundle savedInstance) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder (getActivity());
+        builder.setMessage("Do you want to delete this item?")
+            .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            public void onClick ( DialogInterface dia , int id ) {
+                //Remove the item from the database
+
+            }
+        })
+                .setNegativeButton ("No" , new DialogInterface . OnClickListener () {
+            public void onClick ( DialogInterface dia , int id ) {
+                //User decided not to delete the item
+                dia.cancel();
+            }
+        });
+            return builder.show();
+    }
+}
