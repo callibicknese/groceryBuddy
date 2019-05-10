bde006bedf261019a9b8fbc58865b6fd7b7ee9df
SteveFanegaII <https://github.com/callibicknese/groceryBuddy>
2019-05-10 17:27:17 -0500

    Created a simple fragment for Intro Fragment

---
+++ app/src/main/java/com/example/grocerybuddy/IntroFragment.java
@@ -0,0 +1,28 @@
+package com.example.grocerybuddy;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class IntroFragment extends Fragment {
+
+
+    public IntroFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_intro, container, false);
+        return view;
+    }
+
+}
