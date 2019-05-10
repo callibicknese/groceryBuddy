dd8a5ab0a6d84c0a596addf56f5586c23f2c1e0a
SteveFanegaII <https://github.com/callibicknese/groceryBuddy>
2019-05-10 16:38:57 -0500

    Changed import android.Fragment to androidx.Fragment

--- app/src/main/java/com/example/grocerybuddy/ProductInformation.java
+++ app/src/main/java/com/example/grocerybuddy/ProductInformation.java
@@ -1,12 +1,10 @@
 package com.example.grocerybuddy;
 
-
 import android.os.Bundle;
-import androidx.core.app.Fragment;
+import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 
 /**
  * A simple {@link Fragment} subclass.
