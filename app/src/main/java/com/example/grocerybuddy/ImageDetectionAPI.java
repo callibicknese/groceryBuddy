986acd360d64dc9392371c95ac5bde11ff412fd3
Alexander P Gilfillan <apgilfillan@gmail.com>
2019-05-20 20:44:15 -0500

    Added Google Cloud API for Image Reconigition

--- app/src/main/java/com/example/grocerybuddy/ImageDetectionAPI.java
+++ app/src/main/java/com/example/grocerybuddy/ImageDetectionAPI.java
@@ -17,14 +17,15 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-//TODO Convert to Function that takes the name of a image and returns found information
+//TODO Convert to Function that takes the name of a image and returns found information/boolean
 public class ImageDetectionAPI {
-    public static void main(String... args) throws Exception {
+    public void detectImage(String imageName) throws Exception {
         // Instantiates a client
         try (ImageAnnotatorClient vision = ImageAnnotatorClient.create()) {
 
             // The path to the image file to annotate
-            String fileName = "./resources/wakeupcat.jpg";
+            //TODO make sure it is proper imageName
+            String fileName = "./" + imageName ;
 
             // Reads the image file into memory
             Path path = Paths.get(fileName);
@@ -46,11 +47,13 @@ public class ImageDetectionAPI {
             List<AnnotateImageResponse> responses = response.getResponsesList();
 
             for (AnnotateImageResponse res : responses) {
+                //TODO if error inform and tell user to manually enter
                 if (res.hasError()) {
-                    System.out.printf("Error: %s\n", res.getError().getMessage());
+                    //System.out.printf("Error: %s\n", res.getError().getMessage());
                     return;
                 }
 
+                //TODO if no error, then we need to parse out the annotations
                 for (EntityAnnotation annotation : res.getLabelAnnotationsList()) {
                     //annotation.getAllFields().forEach((k, v) ->
                             //System.out.printf("%s : %s\n", k, v.toString()));
